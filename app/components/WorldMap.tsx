23cd5abcf0625e889ee32ca37da7ba786552e0df
Thomas <thomas3christensen@gmail.com>
2026-06-16 19:25:45 -0400

    fix: explicit any types for react-simple-maps geographies

--- app/components/WorldMap.tsx
+++ app/components/WorldMap.tsx
@@ -49,8 +49,8 @@ export default function WorldMap({ destinations }: { destinations: DestinationPi
         className="w-full h-auto"
       >
         <Geographies geography={GEO_URL}>
-          {({ geographies }) =>
-            geographies.map((geo) => (
+          {({ geographies }: { geographies: any[] }) =>
+            geographies.map((geo: any) => (
               <Geography
                 key={geo.rsmKey}
                 geography={geo}
