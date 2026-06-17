e96fbcb2dee5c852ac58581f218339e6b06a9b6d
Thomas <thomas3christensen@gmail.com>
2026-06-16 20:36:41 -0400

    fix: pass secret explicitly to withAuth middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,9 @@
 import { withAuth } from "next-auth/middleware";
 
+const SECRET = process.env.NEXTAUTH_SECRET ?? "b0842e95caa859a2fdcc0da3f8b57101e35442942f34f5a8d371995aeac8db09";
+
 export default withAuth({
+  secret: SECRET,
   pages: {
     signIn: "/login",
   },
